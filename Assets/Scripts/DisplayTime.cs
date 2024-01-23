7aff6dbf2d296263a8710756b51bd2350857321b
EcchiClone <aa01092308481@gmail.com>
2024-01-23 11:44:12 +0900

    Feat: 화면에 현재 시간 표시
    - 선택과제 1번 구현
    - 폰트 개선 시간 부족으로 보류

---
+++ Assets/Scripts/DisplayTime.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using TMPro;
+using System;
+public class DisplayTime : MonoBehaviour
+{
+    public TMP_Text timeText;
+    void Update()
+    {
+        // HH:mm:ss 형식으로 시간 가져오기
+        string time = DateTime.Now.ToString("HH:mm:ss");
+        // 텍스트 컴포넌트에 현재 시간 표시
+        timeText.text = time;
+    }
+}
